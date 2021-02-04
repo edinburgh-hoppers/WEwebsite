b9a41795547a648ff814e61fd47aaafb0ed3f616
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-04 13:25:59 +0400

    #3
    have some functionality on background - not perfect

--- scripts/background.js
+++ scripts/background.js
@@ -4,38 +4,69 @@ var canvas = document.getElementById('background');
 var ctx = canvas.getContext('2d');
 canvas.width  = window.innerWidth;
 canvas.height = window.screen.height;
+let y = 0;
+let flag = true;
 
-/*title loading animation*/
 window.addEventListener('load', (event) => {
     startAnimate();
   });
   
 function startAnimate(){
+    let backgroundFill = window.setInterval(function(){
+        if(y > canvas.height){
+            clearInterval(backgroundFill);
+        }
+        if(flag){
+            let x = 0;
+            let dx = Math.floor(canvas.width/10);
+            console.log(canvas.width)
+            let oneLine = window.setInterval(function(){
+                if(x + dx > canvas.width){
+                    console.log("Entered")
+                    console.log(x+dx)
+                    ctx.beginPath();
+                    ctx.lineWidth=100;
+                    ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
+                    ctx.moveTo(x,y);
+                    ctx.lineTo(x+dx,y);
+                    ctx.stroke();
+                    clearInterval(oneLine);
+                }
+                ctx.beginPath();
+                ctx.lineWidth=100;
+                ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
+                ctx.moveTo(x,y);
+                ctx.lineTo(x + dx,y);
+                ctx.stroke();
+                x = x + dx;
+            }, 30);
+            
+            flag = false;
+        }  
+        else{
+            let x = canvas.width;
+            let dx = Math.floor(canvas.width/10);
+            let oneLine = window.setInterval(function(){
+                if(x < 0){
+                    clearInterval(oneLine);
+                }
+                ctx.beginPath();
+                ctx.lineWidth=100;
+                ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
+                ctx.moveTo(x,y);
+                ctx.lineTo(x - dx,y);
+                ctx.stroke();
+                x = x - dx;
+            }, 30);
+            // y = y + 100
+            flag = true;
+        } 
+        y = y + 100
+    }, 315); // had to extra 10 ms here to prevent y from incrementing before interval ended.
+
     window.setTimeout(function(){
         website.style.display = "block";
         website.style.transition = "opacity 0.8s";
         website.style.opacity = "1";
-    },800);
-}
-ctx.fillStyle = 'orange';
-        ctx.beginPath();
-        ctx.moveTo(100, 100);
-        ctx.moveTo(100, 400);
-        ctx.moveTo(300, 250);
-        ctx.fill();
-
-//actual canvas funciton
-var sWidth = canvas.width;
-var sHeight = canvas.height;
-console.log(sWidth);
-for(let i = 10; i < sWidth; i = i + (sWidth/20)*1){
-    for(let j = 20; j < sHeight; j = j + (sHeight/10)*1){
-        console.log(j);
-        ctx.fillStyle = 'rgba(255, 187, 54,0.4)';
-        ctx.beginPath();
-        ctx.moveTo(i, j);
-        ctx.lineTo(i, j + sWidth/20);
-        ctx.lineTo(i+(sHeight/10), j+ sWidth/20);
-        ctx.fill();
-    }
+    },1500);
 }
