6c0deaf8db848ee331e6005e72560d72100a2dc9
Ap0806 <57600509+Ap0806@users.noreply.github.com>
2021-02-08 14:35:10 +0400

    more for background

--- scripts/background.js
+++ scripts/background.js
@@ -14,23 +14,6 @@ else{
 let y = -200;
 // let flag = true;
 let r = 0;
-// var img = new Image;
-// img.src = '../images/background.png';
-
-// function animate(x,y){
-//     ctx.clearRect(0,0,canvas.width, canvas.height);
-//     ctx.drawImage(img, x, y, 20, 20);
-//     y+= 1;
-//     if(y > canvas.height){
-//         cancelAnimationFrame(triangle);
-//     }
-//     // ctx.rotate(angle*Math.PI);
-//     // angle += 0.1;
-//     // if(angle > 2){
-//     //     angle = 0.1;
-//     // }
-//     triangle = requestAnimationFrame(animate(x+1, y));
-// }
 
 window.addEventListener('load', (event) => {
     startBackground();
@@ -44,25 +27,13 @@ function startBackground(){
         if(r > canvas.width/2 + 400 ){
             clearInterval(fillBgd);
             let tx = -10, ty = canvas.height/4;
-            let bx = canvas.width - ty, by = canvas.height;
+            let bx = canvas.width - ty - 10, by = canvas.height;
             let ribbon = window.setInterval(function(){
                 if(ty < 0){
                     clearInterval(ribbon);
                 }
-                ctx.beginPath();
-                ctx.lineCap = "round";
-                ctx.lineWidth=lwidth;
-                ctx.strokeStyle = "#832b99";
-                ctx.moveTo(tx,ty);
-                ctx.lineTo(tx+10,ty-10);
-                ctx.stroke();
-
-
-                ctx.beginPath();
-                ctx.lineCap = "round";
-                ctx.moveTo(bx,by);
-                ctx.lineTo(bx+10,by-10);
-                ctx.stroke();
+                drawLine(tx,ty,lwidth,'#832b99');
+                drawLine(bx,by, lwidth, '#832b99');
                 bx+=10;
                 by-=10;
                 tx+=10;
@@ -74,64 +45,6 @@ function startBackground(){
         ctx.fill();
         r = r + 10;
     },20);
-
-
-    /*lines filling screen background */
-    // let backgroundFill = window.setInterval(function(){
-    //     if(y > canvas.height){
-    //         clearInterval(backgroundFill);
-    //     }
-    //     if(flag){
-    //         let x = 0;
-    //         let dx = Math.floor(canvas.width/10);
-    //         console.log(canvas.width)
-    //         let oneLine = window.setInterval(function(){
-    //             if(x + dx > canvas.width){
-    //                 // console.log("Entered")
-    //                 // console.log(x+dx)
-                    // ctx.beginPath();
-                    // ctx.lineWidth=200;
-                    // ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
-                    // ctx.moveTo(x,y);
-                    // ctx.lineTo(x+dx,y);
-                    // ctx.stroke();
-    //                 clearInterval(oneLine);
-    //             }
-    //             ctx.beginPath();
-    //             ctx.lineWidth=200;
-    //             ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
-    //             ctx.moveTo(x,y);
-    //             console.log(x,y)
-    //             ctx.lineTo(x + dx,y);
-    //             ctx.stroke();
-    //             x = x + dx;
-    //         }, 20);
-            
-    //         flag = false;
-    //     }  
-    //     else{
-    //         let x = canvas.width;
-    //         let dx = Math.floor(canvas.width/10);
-    //         let oneLine = window.setInterval(function(){
-    //             if(x < 0){
-    //                 clearInterval(oneLine);
-    //             }
-    //             ctx.beginPath();
-    //             ctx.lineWidth=200;
-    //             ctx.strokeStyle = "rgba(255, 187, 54,0.4)";
-    //             ctx.moveTo(x,y);
-    //             ctx.lineTo(x - dx,y);
-    //             ctx.stroke();
-    //             x = x - dx;
-    //         }, 20);
-    //         // y = y + 100
-    //         flag = true;
-    //     } 
-    //     y = y + 200
-    // }, 210); // had to extra 10 ms here to prevent y from incrementing before interval ended.
-//end of lines filing screen background 
-
-
     window.setTimeout(function(){
         website.style.display = "block";
         website.style.transition = "opacity 0.8s";
@@ -141,4 +54,13 @@ function startBackground(){
     
 }
 
+function drawLine(x,y,w,c){
+    ctx.beginPath();
+    ctx.lineCap = "round";
+    ctx.lineWidth=w;
+    ctx.strokeStyle = c;
+    ctx.moveTo(x,y);
+    ctx.lineTo(x+10,y-10);
+    ctx.stroke();
+}
 
